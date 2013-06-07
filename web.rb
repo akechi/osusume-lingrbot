d031c41772cc908a275e79840169433f04eb5415
ujihisa <ujihisa@gmail.com>
2013-06-06 17:41:26 -0700

    check if the delete succeeded/failed

--- web.rb
+++ web.rb
@@ -101,9 +101,14 @@ end
 post '/delete' do
   content_type :json
   bot_verifier = Digest::SHA1.hexdigest("osusume" + ENV["OSUSUME_BOT_SECRET"])
-  osusume({"text" => "!osusume! #{params[:name]}"})
-  open "http://lingr.com/api/room/say?room=computer_science&bot=osusume&text=#{CGI.escape("'#{params[:name]}' がたぶんWebから削除されました")}&bot_verifier=#{bot_verifier}"
-  '{"status": "OK"}'
+  result = osusume({"text" => "!osusume! #{params[:name]}"})
+  if /^Deleted / =~ result # TODO better success/fail handling
+    open "http://lingr.com/api/room/say?room=computer_science&bot=osusume&text=#{CGI.escape("'#{params[:name]}' がたぶんWebから削除されました")}&bot_verifier=#{bot_verifier}"
+    '{"status": "OK"}'
+  else
+    status 404
+    "the osusume isn't on my DB"
+  end
 end
 
 post '/api' do
