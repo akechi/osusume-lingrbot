360e506efabba6e5674d42c404aca26673cdf2e9
mattn <mattn.jp@gmail.com>
2014-11-12 16:44:28 +0900

    realvimtutor

--- web.rb
+++ web.rb
@@ -387,6 +387,13 @@ post '/api' do
   {osusume: "#{result}"}.to_json
 end
 
+post '/realvimtutor' do
+  content_type :json
+  result = Web.osusume({"text"=> "#{params[:user]} < Help me, Real Vimtutors!!!", true)
+  open "http://lingr.com/api/room/say?room=vim&bot=osusume&text=#{urlencode("#{params[:text].inspect} => #{result.inspect} from #{request.env['HTTP_X_REAL_IP']}")}&bot_verifier=#{BOT_VERIFIER}"
+  {osusume: "#{result}"}.to_json
+end
+
 post '/lingr' do
   return "" unless request.ip == LINGR_IP
   json = JSON.parse(request.body.string)
